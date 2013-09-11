7539fc59cff16bbf70e32bd4fbe269a4fc60b0c3
Alan Stanley <astanley@upei.ca>
2013-09-11 15:32:26 -0300

    population of apparatus

--- theme/islandora_transcription_object.tpl.php
+++ theme/islandora_transcription_object.tpl.php
@@ -10,9 +10,7 @@ module_load_include('inc', 'islandora', 'includes/breadcrumb');
 drupal_set_breadcrumb(islandora_get_breadcrumbs($object));
 drupal_set_title($object->label);
 ?>
-<div class ="versionable_object_header">
-  <?php print "<h1>" . $object->label . "</h12>"; ?>
-</div>
+
 <div class="islandora_transcription_object">
   <?php if (isset($transcription)): ?>
     <?php print $transcription; ?>
--- theme/islandora_versionable_object.tpl.php
+++ theme/islandora_versionable_object.tpl.php
@@ -11,18 +11,19 @@ $transcriptions = array_keys($members, 'islandora:transcriptionCModel');
 drupal_set_breadcrumb(islandora_get_breadcrumbs($object));
 drupal_set_title($object->label);
 ?>
-<div class ="versionable_object_header">
-  <?php print "<h1>" . $object->label . "</h12>"; ?>
-</div>
+
 <div class="islandora_cwrcwriter_object">
+  <?php if (!$critical_edition  && empty($transcriptions)): ?>
+    <?php print t("This Verionable object has no associated objects."); ?>
+  <?php endif; ?>
   <?php if ($critical_edition): ?>
-    <?php print l($edition_object->label, "islandora/object/${critical_edition}");  ?>
+    <?php print l($edition_object->label, "islandora/object/${critical_edition}"); ?>
   <?php endif; ?>
 </div>
 
 <div class ="islandora_critical_transcriptions" >
   <?php foreach ($transcriptions as $transcription): ?>
-    <?php 
+    <?php
     $transcription_object = islandora_object_load($transcription);
     print l($transcription_object->label, "islandora/object/$transcription_object");
     print "<br />";
